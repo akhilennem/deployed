71fe9ecf40d3a0df37e70ab1fe5bed9753d2a041
akhilennem <105033535+akhilennem@users.noreply.github.com>
2022-07-11 15:08:59 +0530

    Update AuthController.java

--- src/main/java/com/attendance/login/UserPackage/controllers/AuthController.java
+++ src/main/java/com/attendance/login/UserPackage/controllers/AuthController.java
@@ -65,10 +65,17 @@ public class AuthController {
         .map(item -> item.getAuthority())
         .collect(Collectors.toList());
 
-    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
-        .body(new UserInfoResponse(userDetails.getUsername(),
-                                   userDetails.getEmail(),
-                                   roles));
+      if (roles.toString().equals("[ROLE_USER]")){
+      return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    } else if (roles.toString().equals("[ROLE_ADMIN]")) {
+      return new ResponseEntity<>(HttpStatus.ALREADY_REPORTED);
+
+    }
+    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString(), String.valueOf(HttpStatus.OK))
+            .body(new UserInfoResponse(
+                    userDetails.getUsername(),
+                    userDetails.getEmail(),
+                    roles));
   }
 
   @PostMapping("/signup")
@@ -132,6 +139,58 @@ public class AuthController {
     public Role addrole(Role role)
   {
     return roleRepository.save(role);
+  }
+  
+   @PostMapping("/admin-signup")
+  public ResponseEntity<?> registerAdmin(@Valid @RequestBody SignupRequest signUpRequest) {
+    if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+    }
+
+    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+    }
+
+    // Create new user's account
+    User user = new User(signUpRequest.getUsername(),
+            signUpRequest.getEmail(),
+            encoder.encode(signUpRequest.getPassword()));
+
+    Set<String> strRoles = signUpRequest.getRole();
+    Set<Role> roles = new HashSet<>();
+
+    if (strRoles == null) {
+      Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+      roles.add(adminRole);
+    } else {
+      strRoles.forEach(role -> {
+        switch (role) {
+          case "user":
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+
+            break;
+          case "mod":
+            Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(modRole);
+
+            break;
+          default:
+            Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(adminRole);
+        }
+      });
+    }
+    user.setRoles(roles);
+    userRepository.save(user);
+
+    return ResponseEntity.ok(new MessageResponse("Registered successfully!"));
+//
+//  }
   }
 
 
