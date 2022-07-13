82b45f0027d9eeb95c9b8925a63ee797ce0ad816
akhilennem <105033535+akhilennem@users.noreply.github.com>
2022-07-13 12:40:06 +0530

    Update AuthController.java

--- src/main/java/com/attendance/login/UserPackage/controllers/AuthController.java
+++ src/main/java/com/attendance/login/UserPackage/controllers/AuthController.java
@@ -81,11 +81,11 @@ public class AuthController {
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
     if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-      return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+      return ResponseEntity.unprocessableEntity().body(new MessageResponse("Error: Username is already taken!"));
     }
 
     if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-      return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+      return ResponseEntity.unprocessableEntity().body(new MessageResponse("Error: Email is already in use!"));
     }
 
     // Create new user's account
